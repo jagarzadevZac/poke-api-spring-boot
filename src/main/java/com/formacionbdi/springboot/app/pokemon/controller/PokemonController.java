9ed32ec867480a409c09e9361f428ada5c2117ca
jesus19951206@gmail.com <jesus19951206@gmail.com>
2022-02-10 20:16:41 -0600

    last changes

--- src/main/java/com/formacionbdi/springboot/app/pokemon/controller/PokemonController.java
+++ src/main/java/com/formacionbdi/springboot/app/pokemon/controller/PokemonController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +23,7 @@ import org.springframework.http.MediaType;
 
 @RestController
 @RequestMapping("/pokemon")
+@CrossOrigin
 public class PokemonController {
 
 	@Autowired
